135720ad6901cac41ae900014d5140c5d26f4e8b
vissarion tsitoglou <vissarion.tsitoglou@pan-net.eu>
2020-10-24 20:30:14 +0200

    minor fixes

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,3 +1,4 @@
-module.exports = (err:any, _req: any, res:any) => {
+// eslint-disable-next-line no-unused-vars
+module.exports = (err:any, _req: any, res:any, _next: any) => {
   res.status(400).send(err.message);
 };
